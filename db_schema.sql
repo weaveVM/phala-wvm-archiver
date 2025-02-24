013b0e55fa9204e9e879998942e016e5604951a1
charmful0x <charmful0x@gmail.com>
2025-02-24 18:30:42 +0100

    feat: config phala

--- db_schema.sql
+++ db_schema.sql
@@ -1,19 +1,19 @@
-DROP TABLE IF EXISTS WeaveVMArchiver;
-DROP TABLE IF EXISTS WeaveVMArchiverBackfill;
+DROP TABLE IF EXISTS WeaveVMArchiverPhala;
+DROP TABLE IF EXISTS WeaveVMArchiverPhalaBackfill;
 
-CREATE TABLE IF NOT EXISTS WeaveVMArchiver (
+CREATE TABLE IF NOT EXISTS WeaveVMArchiverPhala (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     NetworkBlockId INT UNIQUE,
     WeaveVMArchiveTxid VARCHAR(66) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS WeaveVMArchiverBackfill (
+CREATE TABLE IF NOT EXISTS WeaveVMArchiverPhalaBackfill (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     NetworkBlockId INT UNIQUE,
     WeaveVMArchiveTxid VARCHAR(66) UNIQUE
 );
 
-CREATE INDEX idx_archiver_txid ON WeaveVMArchiver (WeaveVMArchiveTxid);
-CREATE INDEX idx_backfill_txid ON WeaveVMArchiverBackfill (WeaveVMArchiveTxid);
-CREATE INDEX idx_archiver_block_id ON WeaveVMArchiver (NetworkBlockId);
-CREATE INDEX idx_backfill_block_id ON WeaveVMArchiverBackfill (NetworkBlockId);
+CREATE INDEX idx_archiver_txid ON WeaveVMArchiverPhala (WeaveVMArchiveTxid);
+CREATE INDEX idx_backfill_txid ON WeaveVMArchiverPhalaBackfill (WeaveVMArchiveTxid);
+CREATE INDEX idx_archiver_block_id ON WeaveVMArchiverPhala (NetworkBlockId);
+CREATE INDEX idx_backfill_block_id ON WeaveVMArchiverPhalaBackfill (NetworkBlockId);
